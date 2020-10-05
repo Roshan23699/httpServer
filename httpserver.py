e963a6f71746f79e99525270beb26b6377a999c6
Aditya190920 <adiabhang04@gmail.com>
2020-10-05 10:30:06 +0530

    connection has been made with client

--- httpserver.py
+++ httpserver.py
@@ -6,6 +6,17 @@ host = '127.0.0.1'
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 port = int(sys.argv[1])
-server_socket.bind((host, port))
+server_socket.bind(('', port))
 server_socket.listen(1)
 print("Server is ready to listen")
+
+
+while True:
+    client, addr = server_socket.accept()
+    print("connection has recieved from ip", addr[0])
+    print("on port", addr[1])
+    msg = client.recv(1024).decode('utf-8')
+    dict1 = msg.split()
+    print(l1)
+    
+    break
