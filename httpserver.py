c2f69756de7e6176b9631fe281f946423931fda5
Adi190920 <65355691+Adi190920@users.noreply.github.com>
2020-10-05 09:03:44 +0530

    Update httpserver.py

--- httpserver.py
+++ httpserver.py
@@ -1,1 +1,11 @@
 
+from socket import *
+import sys
+
+host = '127.0.0.1'
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+port = int(sys.argv[1])
+server_socket.bind((host, port))
+server_socket.listen(1)
+print("Server is ready to listen")
