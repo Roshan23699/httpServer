c5cf89ae96764ed9c3a4b4a95d4ca13228e67cde
Roshan Bangar <roshanbangar21@gmail.com>
2020-10-09 09:09:31 +0530

    added date and time

--- httpserver.py
+++ httpserver.py
@@ -1,14 +1,14 @@
-
 from socket import *
 import sys
 import os
+import datetime
 
 host = '127.0.0.1'
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 port = int(sys.argv[1])
 server_socket.bind(('', port))
-server_socket.listen(1)
+server_socket.listen(2)
 print("Server is ready to listen")
 
 
@@ -27,7 +27,8 @@ while True:
             if os.path.exists(dict1[1]):
                 requested_file = open(dict1[1], 'r')
                 response += "HTTP/1.1 200 OK\n"
-                response += "Date: Thu, 08 Oct 2020 11:10:22 GMT\n"
+                curr_time = datetime.datetime.now()
+                response += ("Date: " + curr_time.strftime("%A") + ", "+ curr_time.strftime("%d") + " " +  curr_time.strftime("%b") + " " + curr_time.strftime("%Y") + " " + curr_time.strftime("%X") + " GMT\n")
                 response += "Server: Apache/2.4.29 (Ubuntu)\n"
                 response += "Last-Modified: Sat, 14 Sep 2019 10:14:08 GMT\n"
                 response += 'ETag: "2aa6-59280a1a3740c"\n'
