e73a97cc284352464847e145acd0bf1932327ef1
Roshan23699 <roshanbangar21@gmail.com>
2020-11-09 17:59:52 +0530

    Conditional

--- server/request_GET.py
+++ server/request_GET.py
@@ -24,46 +24,47 @@ def request_GET(headers, client, addr, parser):
             #return 401 Unauthorized
             unauthorized(headers, client, addr, parser)
         else :
-            # if conditional_check(headers):
-            if False:
+            if conditional_check(headers):
+            # if False:
                 response += "HTTP/1.1 304 Not Modified\n"
             else :
                 response += "HTTP/1.1 200 OK\n"
-                if checkCookie(headers):
-                    #don't send cookie for now
-                    # print("\n\n\n\n")
-                    # print(headers)
-                    # print("\n\n\n\n")
-                    pass
-                else :
-                    response = setCookie(response)
-                curr_time = datetime.datetime.now()
-                response += ("Date: " + curr_time.strftime("%A") + ", "+ curr_time.strftime("%d") + " " +  curr_time.strftime("%b") + " " + curr_time.strftime("%Y") + " " + curr_time.strftime("%X") + " GMT\n")
-                response += "Server: Aditya-Roshan/1.0.0 (Cn)\n"
-                last_modified = os.path.getmtime(headers['request-uri'])
-                response += ("last-Modified: " + datetime.datetime.fromtimestamp(last_modified).strftime("%A, %d %b, %Y %I:%M:%S")+ " GMT\n")
-                if 'Etag' in headers:
-                    response += 'ETag: ' + headers['Etag']+ '\n'
-                response += "Accept-Ranges: bytes\n"
-                content_length = os.path.getsize(headers['request-uri'])
-                response += "Content-Length: " + str(content_length) + "\n"
-                response += "Vary: Accept-Encoding\n"
-                if content_type == None:
-                    content_type = "text/html"
-                if 'Accept' in headers:
-                    if not ('*/*' in headers['Accept']  or content_type in headers['Accept']):
-                        bad_request(headers,client, addr, parser)
-                response += "Content-Type: " + content_type + "\n"
-                if 'Connection' in headers and headers['Connection'] != "keep-alive":
-                    response += "keep-alive: timeout=5, max=100\nConnection: Keep-Alive\n\n"
-                else :
-                    response += "\n"
-                response  = response.encode()
-                response += read_file(headers['request-uri'], content_type)
-                client.send(response)
-                custom_log(client, addr, curr_time, headers, parser, '200', str(content_length))
-                if 'Connection' in headers and headers['Connection'] != "keep-alive":
-                        client.close()  
+            if checkCookie(headers):
+                #don't send cookie for now
+                # print("\n\n\n\n")
+                # print(headers)
+                # print("\n\n\n\n")
+                pass
+            else :
+                response = setCookie(response)
+            curr_time = datetime.datetime.now()
+            response += ("Date: " + curr_time.strftime("%A") + ", "+ curr_time.strftime("%d") + " " +  curr_time.strftime("%b") + " " + curr_time.strftime("%Y") + " " + curr_time.strftime("%X") + " GMT\n")
+            response += "Server: Aditya-Roshan/1.0.0 (Cn)\n"
+            last_modified = os.path.getmtime(headers['request-uri'])
+            response += ("last-Modified: " + datetime.datetime.fromtimestamp(last_modified).strftime("%A, %d %b, %Y %I:%M:%S")+ " GMT\n")
+            if not 'If-None-Match' in headers:
+                response += 'ETag: ' + etag(headers['request-uri']) + '\n'
+                response = setCasheControl(response, maxAge=10)
+            response += "Accept-Ranges: bytes\n"
+            content_length = os.path.getsize(headers['request-uri'])
+            response += "Content-Length: " + str(content_length) + "\n"
+            response += "Vary: Accept-Encoding\n"
+            if content_type == None:
+                content_type = "text/html"
+            if 'Accept' in headers:
+                if not ('*/*' in headers['Accept']  or content_type in headers['Accept']):
+                    bad_request(headers,client, addr, parser)
+            response += "Content-Type: " + content_type + "\n"
+            if 'Connection' in headers and headers['Connection'] != "keep-alive":
+                response += "keep-alive: timeout=5, max=100\nConnection: Keep-Alive\n\n"
+            else :
+                response += "\n"
+            response  = response.encode()
+            response += read_file(headers['request-uri'], content_type)
+            client.send(response)
+            custom_log(client, addr, curr_time, headers, parser, '200', str(content_length))
+            if 'Connection' in headers and headers['Connection'] != "keep-alive":
+                    client.close()  
                     
     else :
         not_found(headers, client, addr, parser)
--- server/request_conditional.py
+++ server/request_conditional.py
@@ -3,9 +3,18 @@ import sys
 import os
 import datetime
 from support_functions import *
+import hashlib 
+
+def setCasheControl(response, maxAge):
+	response += "Cache-Control: max-age=" + str(maxAge) + "\n"
+	return response
+
+def etag(resource):
+	#for etag md5 hash is used
+	return (hashlib.md5(resource.encode())).hexdigest()
 
 def check_etag(et, resource) :
-	if etag is not None and et == etag(resource):
+	if et is not None and et == etag(resource):
 		return True
 	else :
 		return False
@@ -14,9 +23,12 @@ def check_etag(et, resource) :
 def conditional_check(headers):
 	#for now just checking based on etag
 	resource = headers['request-uri']
-	if 'Etag' in headers:
-		etag = headers['Etag']
-	if check_etag(etag, resource):
-		return True
-	else :
-		return False
+	if 'If-None-Match' in headers:
+		et = headers['If-None-Match']
+		print("\n\n\n")
+		print(et)
+		print(etag(resource))
+		print("\n\n\n")
+		if check_etag(et, resource):
+			return True
+	return False
