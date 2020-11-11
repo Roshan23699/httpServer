155be63259ae105cec282f27e5b4654574e4998c
Roshan23699 <roshanbangar21@gmail.com>
2020-11-11 11:24:20 +0530

    stress test

--- server/authorization.py
+++ server/authorization.py
@@ -14,7 +14,20 @@ def authorize(headers, client, addr, parser):
 	#Here we need to check authorization but just checking if client has send the authorization then
 	#assuming that its true
 	
-	if 'Authorization' in headers and headers['Authorization']== None:
-		return False
-	else:
-		 return True
+	if 'Authorization' in headers and headers['Authorization'] is not None:
+		credentials = headers['Authorization']
+		credentials = credentials.split('&')
+		for _ in credentials:
+			_ = _.split('=')
+		username = None
+		password = None
+		if credentials[0][0] == 'username':
+			username = credentials[0][1]
+			password = credentials[1][1]
+		elif credentials[1][0] == 'username':
+			username = credentials[1][1]
+			password = credentials[0][1]
+
+		if username is not None and parser.get('credentials', username):
+			return True
+	return False
--- server/tests.py
+++ server/tests.py
@@ -2,8 +2,10 @@ import requests
 from  time import time
 from threading import Thread
 import sys
+#stress test
 def test_GET(no_of_requests_at_a_time):
 	url = 'http://127.0.0.1:12000/volim/'
+	data = {'somekey': 'somevalue'}
 	start = time()
 	for _ in range(no_of_requests_at_a_time):
 		new_thread = Thread(target=send_GET, args=[url, ])
@@ -12,8 +14,72 @@ def test_GET(no_of_requests_at_a_time):
 	return end - start
 
 
+#stress test
+def test_PUT(no_of_requests_at_a_time):
+	url = 'http://127.0.0.1:12000/volim/'
+	data = {'somekey': 'somevalue'}
+	start = time()
+	for _ in range(no_of_requests_at_a_time):
+		new_thread = Thread(target=send_PUT, args=[url, data, ])
+		new_thread.start()
+	end = time()
+	return end - start
+
+
+#stress test
+def test_DELETE(no_of_requests_at_a_time):
+	data = {'somekey': 'somevalue'}
+	url = 'http://127.0.0.1:12000/volim/'
+	start = time()
+	for _ in range(no_of_requests_at_a_time):
+		new_thread = Thread(target=send_DELETE, args=[url, ])
+		new_thread.start()
+	end = time()
+	return end - start
+
+def test_POST(no_of_requests_at_a_time):
+	data = {'somekey': 'somevalue'}
+	url = 'http://127.0.0.1:12000/volim/'
+	start = time()
+	for _ in range(no_of_requests_at_a_time):
+		new_thread = Thread(target=send_POST, args=[url, data, ])
+		new_thread.start()
+	end = time()
+	return end - start
+
+def test_HEAD(no_of_requests_at_a_time):
+	data = {'somekey': 'somevalue'}
+	url = 'http://127.0.0.1:12000/volim/index.html'
+	start = time()
+	for _ in range(no_of_requests_at_a_time):
+		new_thread = Thread(target=send_HEAD, args=[url, ])
+		new_thread.start()
+	end = time()
+	return end - start
+
+
+
 def send_GET(url):
 	r = requests.get(url)
+	r.close()
+
+
+def send_POST(url, data):
+	r = requests.post(url, data)
+	r.close()
+
+def send_DELETE(url):
+	r = requests.delete(url)
+	r.close()
+
+
+def send_PUT(url, data):
+	r = requests.put(url, data)
+	r.close()
+
+
+def send_HEAD(url):
+	r = requests.head(url)
 	r.close()
 
 if __name__ == "__main__":
