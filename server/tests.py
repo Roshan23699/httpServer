48e8aa13e761eee5610f56b343086dfd28047e87
Roshan23699 <roshanbangar21@gmail.com>
2020-11-11 14:06:09 +0530

    test

--- server/tests.py
+++ server/tests.py
@@ -2,6 +2,7 @@ import requests
 from  time import time
 from threading import Thread
 import sys
+import unittest
 #stress test
 def test_GET(no_of_requests_at_a_time):
 	url = 'http://127.0.0.1:12000/volim/'
@@ -82,7 +83,33 @@ def send_HEAD(url):
 	r = requests.head(url)
 	r.close()
 
+
+
+
+
+
+#confirmance tests
+class Test_GET(unittest.TestCase):
+	def test_status_code(self, clientSocket):
+		
+		pass
+
+
 if __name__ == "__main__":
+	#client for confirmance tests
+	from socket import *
+	serverName = '127.0.0.1'
+	serverPort = 12000
+	#clientPort = 12004
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+	#clientSocket.bind((1, clientSocket));
+	#above commented lines resulted in error binding failed
+	clientSocket.connect((serverName,serverPort))
+	# sentence = raw_input('Input lowercase sentence:')
+	# clientSocket.send(sentence.encode())
+	# modifiedSentence = clientSocket.recv(1024)
+	# print('From Server: ', modifiedSentence.decode())
+	# clientSocket.close()
 	time = test_GET(300)
 	print(time)
 	sys.exit(0)
